0ec6a54bd5a68937c83a7ac8d32a3962cefa4524
luis <luis.sg.vitor@gmail.com>
2021-02-14 22:04:02 -0300

    Hashing password and fixing bugs

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 import knex from '../database/connection'
 import registerValidation from '../services/validation'
+import bcrypt from 'bcrypt'
 
 class UserController {
   async create(request: Request, response: Response) {
@@ -11,19 +12,28 @@ class UserController {
     if (error) return response.status(400).json({ message: error.details[0].message })
 
     // check if username or email already exists
-
+    console.log(username)
+    console.log(email)
     const user = await knex('users').where('username', username).orWhere('email', email).select('*')
-    if (user) return response.status(400).json({ error: 'username or email already exists' })
+    console.log(user)
+    if (user[0]) {
+      return response.status(400).json({ error: 'username or email already exists' })
+    }
+    
 
-    // create user
-    // await knex('users').insert({
-    //     name, 
-    //     username, 
-    //     email, 
-    //     password
-    // })
+    // hashing password
+    const salt = await bcrypt.genSalt(10)
+    const hashedpassword = await bcrypt.hash(password, salt)
 
-    // return response.json({ message: 'success' })
+    //create user
+    await knex('users').insert({
+        name, 
+        username, 
+        email, 
+        password: hashedpassword
+    })
+
+    return response.json({ message: 'success' })
   }
 }
 
