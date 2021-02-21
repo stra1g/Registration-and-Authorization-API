abc2f975d51c63494e60012e683ac6036f7f9429
luis <luis.sg.vitor@gmail.com>
2021-02-21 11:37:22 -0300

    setting cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express'
 import routes from './routes'
 import boom from 'express-boom'
+import cors from 'cors'
 
 import 'dotenv/config'
 
 const app = express()
 
+app.use(cors())
 app.use(boom())
 app.use(express.json())
 app.use(routes)
