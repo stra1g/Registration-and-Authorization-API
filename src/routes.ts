5975a9356a8c5d83cf19267411c03f0bd19dd052
luis <luis.sg.vitor@gmail.com>
2021-03-23 12:34:34 -0300

    Creating sendResetLink route

--- src/routes.ts
+++ src/routes.ts
@@ -4,16 +4,21 @@ import { auth } from './middlewares/auth'
 import UserController from './controllers/UserController'
 import AuthController from './controllers/AuthController'
 import TestController from './controllers/TestController'
+import ResetPasswordController from './controllers/ResetPasswordController'
 
 const routes = express.Router()
 const userController = new UserController()
 const authController = new AuthController()
 const testController = new TestController()
+const resetPasswordController = new ResetPasswordController()
 
 routes.post('/register', userController.create)
 routes.get('/register/checkEmail', userController.findByEmail)
 routes.get('/register/checkUsername', userController.findByUsername)
+
 routes.post('/login', authController.login)
+
+routes.post('/reset-password', resetPasswordController.sendResetLink)
 
 routes.use(auth)
 
